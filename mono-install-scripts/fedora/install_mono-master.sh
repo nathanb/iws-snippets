2233f9b88a09eb73c96284758ce9ecac1488357c
Nathan Bridgewater <nathan@integratedwebsystems.com>
2011-06-01 21:42:45 -0500

    added experimental fedora scripts for master mono

---
+++ mono-install-scripts/fedora/install_mono-master.sh
@@ -0,0 +1,75 @@
+#!/bin/bash -e
+
+TOPDIR=$(pwd)
+BUILDDIR=$TOPDIR/build
+PREFIX=/opt/mono-2.10
+
+export PATH=$PREFIX/bin:$PATH
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+
+
+echo "updating existing system"
+yum update -y
+
+echo "installing prerequisites"
+yum install -y make automake glibc-devel gcc-c++ gcc glib2-devel pkgconfig subversion bison gettext-libs autoconf httpd httpd-devel libtool wget libtiff-devel libexif-devel libexif libjpeg-devel gtk2-devel atk-devel pango-devel giflib-devel libglade2-devel gettext-devel
+
+mkdir -p $BUILDDIR
+
+echo
+echo "downloading mono packages"
+echo
+
+PROJECTS=("mono"
+"libgdiplus"
+"gtk-sharp"
+"xsp"
+"mod_mono")
+
+SOURCES=("git://github.com/mono/mono.git"
+"git://github.com/mono/libgdiplus.git"
+"git://github.com/mono/gtk-sharp.git"
+"git://github.com/mono/xsp.git"
+"git://github.com/mono/mod_mono.git")
+
+
+cd $BUILDDIR
+
+echo Cloning projects
+for i in "${SOURCES[@]}"
+do
+git clone $i
+done
+
+echo
+echo "building mono packages"
+echo
+
+for i in "${PROJECTS[@]}"
+do
+	cd $BUILDDIR/$i
+	./autogen.sh --prefix=$PREFIX
+	make
+	
+	if [ "$i" = ${PROJECTS[0]} ]
+	then
+		sudo make install
+	fi
+done
+
+echo
+echo "installing mono packages"
+echo
+
+for i in "${PROJECTS[@]:1}"
+do
+	cd $BUILDDIR/$i
+	sudo make install
+done
+
+cd $BUILDDIR
+echo
+echo "done"
+
+
---
+++ mono-install-scripts/fedora/install_monodevelop-master.sh
@@ -0,0 +1,74 @@
+#!/bin/bash -e
+
+TOPDIR=$(pwd)
+BUILDDIR=$TOPDIR/build
+PREFIX=/opt/mono-2.10
+
+export PATH=$PREFIX/bin:$PATH
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+
+
+echo "updating existing system"
+yum update -y
+
+echo "installing prerequisites"
+yum install -y make automake glibc-devel gcc-c++ gcc glib2-devel pkgconfig subversion bison gettext-libs autoconf httpd httpd-devel libtool wget libtiff-devel libexif-devel libexif libjpeg-devel gtk2-devel atk-devel pango-devel giflib-devel libglade2-devel gettext-devel
+
+mkdir -p $BUILDDIR
+
+echo
+echo "downloading mono packages"
+echo
+
+PROJECTS=("mono-addins"
+"debugger"
+"mono-tools"
+"gnome-sharp"
+"monodevelop"
+"gluezilla"
+)
+
+SOURCES=("git://github.com/mono/mono-addins.git"
+"git://github.com/mono/debugger.git"
+"git://github.com/mono/mono-tools.git"
+"git://github.com/mono/gnome-sharp.git"
+"git://github.com/mono/monodevelop.git"
+"git://github.com/mono/gluezilla.git"
+)
+
+
+cd $BUILDDIR
+
+echo Cloning projects
+for i in "${SOURCES[@]}"
+do
+git clone $i
+done
+
+echo
+echo "building mono packages"
+echo
+
+for i in "${PROJECTS[@]}"
+do
+	cd $BUILDDIR/$i
+	./autogen.sh --prefix=$PREFIX
+	make
+done
+
+echo
+echo "installing mono packages"
+echo
+
+for i in "${PROJECTS[@]:1}"
+do
+	cd $BUILDDIR/$i
+	sudo make install
+done
+
+cd $BUILDDIR
+echo
+echo "done"
+
+

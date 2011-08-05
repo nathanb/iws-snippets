1d885852a2451f3f6c4266004d23d9c31b3e0edd
Nathan Bridgewater <nathan@integratedwebsystems.com>
2011-08-04 22:10:19 -0500

    updated fedora script, untested

--- mono-install-scripts/fedora/install_mono-2.10.sh
+++ mono-install-scripts/fedora/install_mono-2.10.sh
@@ -23,16 +23,16 @@ echo
 
 cd $BUILDDIR
 
-PACKAGES=("mono-2.10.2"
+PACKAGES=("mono-2.10.3"
 "libgdiplus-2.10"
-"gtk-sharp-2.12.10"
+"gtk-sharp-2.12.11"
 "xsp-2.10.2"
 "mod_mono-2.10")
 
-URLS=("http://ftp.novell.com/pub/mono/sources/mono/mono-2.10.2.tar.bz2"
-"http://ftp.novell.com/pub/mono/sources/libgdiplus/libgdiplus-2.10.tar.bz2"
-"http://ftp.novell.com/pub/mono/sources/gtk-sharp212/gtk-sharp-2.12.10.tar.bz2"
-"http://ftp.novell.com/pub/mono/sources/xsp/xsp-2.10.2.tar.bz2"
+URLS=("http://download.mono-project.com/sources/mono/mono-2.10.3.tar.bz2"
+"http://download.mono-project.com/sources/libgdiplus/libgdiplus-2.10.tar.bz2"
+"http://download.mono-project.com/sources/gtk-sharp212/gtk-sharp-2.12.11.tar.bz2"
+"http://download.mono-project.com/sources/xsp/xsp-2.10.2.tar.bz2"
 "http://ftp.novell.com/pub/mono/sources/mod_mono/mod_mono-2.10.tar.bz2")
 
 

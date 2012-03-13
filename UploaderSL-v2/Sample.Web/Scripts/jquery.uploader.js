b0f1823387f9e0d03eb6cde9ab09274bd684e617
Nathan Bridgewater <nathan@integratedwebsystems.com>
2012-03-13 11:47:41 -0500

    cleanup js and update to adapt to other offline changes.

--- UploaderSL-v2/Sample.Web/Scripts/jquery.uploader.js
+++ UploaderSL-v2/Sample.Web/Scripts/jquery.uploader.js
@@ -24,6 +24,19 @@
 			this.uploader.settings.starting();
 			return this;
 		},
+		serializeCustomData: function () {
+			var data;
+			data = $(this).uploader.settings.customData;
+			var s = '';
+			var ix = 0;
+			for (property in data) {
+				if (ix > 0)
+					s += ';';
+				s += property + ':' + data[property].toString();
+				ix++;
+			}
+			return s;
+		},
 		option: function (options) {
 			if (options)
 				$.extend($(this).uploader.settings, options);
@@ -61,67 +74,3 @@
 		'chunkSize': 0 //default of 200 KB
 	};
 })(jQuery);
-
-
-function uploader_onSilverlightError (sender, args) {
-	var appSource = "";
-	if (sender != null && sender != 0) {
-		appSource = sender.getHost().Source;
-	}
-	var errorType = args.ErrorType;
-	var iErrorCode = args.ErrorCode;
-
-	var errMsg = "Unhandled Error in Silverlight Application " + appSource + "\n";
-
-	errMsg += "Code: " + iErrorCode + "    \n";
-	errMsg += "Category: " + errorType + "       \n";
-	errMsg += "Message: " + args.ErrorMessage + "     \n";
-
-	if (errorType == "ParserError") {
-		errMsg += "File: " + args.xamlFile + "     \n";
-		errMsg += "Line: " + args.lineNumber + "     \n";
-		errMsg += "Position: " + args.charPosition + "     \n";
-	}
-	else if (errorType == "RuntimeError") {
-		if (args.lineNumber != 0) {
-			errMsg += "Line: " + args.lineNumber + "     \n";
-			errMsg += "Position: " + args.charPosition + "     \n";
-		}
-		errMsg += "MethodName: " + args.methodName + "     \n";
-	}
-
-	throw new Error(errMsg);
-}
-
-//default callbacks for this silverlight control
-function uploader_uploadSequenceProgress (percent, message) {
-	$('#uploaderControl').uploader('sequenceProgress', percent, message);
-}
-
-function uploader_uploadContentProgress(percent, message) {
-	$('#uploaderControl').uploader('contentProgress', percent, message);
-}
-function uploader_uploadComplete (success, message) {
-	$('#uploaderControl').uploader('complete', success, message);
-}
-function uploader_uploadStarting () {
-	$('#uploaderControl').uploader('starting');
-}
-
-function uploader_initSilverlight() {
-	var settings = $('#uploaderControl').uploader.settings;
-	var raw = document.getElementById("uploaderControl");
-	raw.content.page.Setup(settings.url, settings.maxSize, settings.uploadInividually, settings.callbackSequenceProgress, settings.callbackContentProgress, settings.callbackCompleted, settings.callbackStarting, uploader_serializeCustomData(settings.customData), settings.buttonText, settings.uploadChunked, settings.chunkSize);
-}
-
-function uploader_serializeCustomData (data) {
-	var s = '';
-	var ix = 0;
-	for (property in data) {
-		if (ix > 0)
-			s += ';';
-		s += property + ':' + data[property].toString();
-		ix++;
-	}
-	return s;
-}
